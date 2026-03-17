afaa513acd30b720b54ae9dcb5dc7770434f1617
Oscar <albertlund121@gmail.com>
2026-03-17 17:04:06 +0100

    Update destinations table structure and remove old destinations.sql file

--- 2026_1_travel.sql
+++ 2026_1_travel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Mar 04, 2026 at 10:28 AM
+-- Generation Time: Mar 17, 2026 at 04:03 PM
 -- Server version: 10.6.20-MariaDB-ubu2004
 -- PHP Version: 8.3.26
 
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `destinations`
+--
+
+CREATE TABLE `destinations` (
+  `destination_pk` char(32) NOT NULL,
+  `destination_user_fk` char(32) NOT NULL,
+  `destination_title` varchar(100) NOT NULL,
+  `destination_date_from` bigint(20) UNSIGNED NOT NULL,
+  `destination_date_to` bigint(20) UNSIGNED NOT NULL,
+  `destination_description` text DEFAULT NULL,
+  `destination_location` varchar(100) NOT NULL COMMENT '\r\n',
+  `destination_country` varchar(100) NOT NULL,
+  `destination_created_at` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -39,6 +57,12 @@ CREATE TABLE `users` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `destinations`
+--
+ALTER TABLE `destinations`
+  ADD PRIMARY KEY (`destination_pk`);
 
 --
 -- Indexes for table `users`
